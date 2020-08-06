178a4499060f3d7a881df58d4b88860699f773d5
Adam Domoslawski <37457625+RabDou44@users.noreply.github.com>
2020-08-06 13:39:20 +0200

    Update Card.py

--- Card.py
+++ Card.py
@@ -1,15 +1,18 @@
 class Card:
     figures = [str(x) for x in range(2, 11)] + ['J', 'Q', 'K', 'A']
-    suits = ("\u2663", "\u2666", "\u2665", "\u2660", "?")
+    suits = ("\u2663", "\u2666", "\u2665", "\u2660")
 
     def __init__(self, num1, num2):
-        self.__suit = Card.suits[num1]
-        self.__figure = Card.figures[num2]
+        """
+        :param suit: suit in [0,1,2,3]
+        :param figure: figure in [0,1,...,12]
+        """
+        self.__suit = Card.suits[suit]
+        self.__figure = Card.figures[figure]
         self.__pts = 0
         self.__is_ace = False
-        self.__covered = False
-
-        self.__set_points(num2+2)
+        self.__covered = True
+        self.__set_points(figure + 2)
 
     def __str__(self):
         if not self.__covered:
@@ -18,7 +21,10 @@ class Card:
             return 'XX'
 
     def __eq__(self, other):
-        return self.__figure == other.get_figure()
+        check = False
+        if not other.is_covered and not self.is_covered():
+            check = other.get_figure() == self.__figure
+        return check
 
     def __len__(self):
         return self.__pts if not self.__covered else 0
@@ -34,6 +40,9 @@ class Card:
 
     def is_ace(self):
         return self.__is_ace
+    
+    def is_covered(self):
+        return self.__covered
 
     def get_figure(self):
         return self.__figure
